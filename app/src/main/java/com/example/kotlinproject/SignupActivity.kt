d89ed43f7dc7daad3a4aba80121d92f63f621c4f
JustinMcNeal <carononganjustinmcneal24@gmail.com>
2024-08-28 15:56:29 +0800

    fixed xml layouts

--- app/src/main/java/com/example/kotlinproject/SignupActivity.kt
+++ app/src/main/java/com/example/kotlinproject/SignupActivity.kt
@@ -5,9 +5,6 @@ import android.os.Bundle
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import kotlinx.coroutines.*
 
 class SignupActivity : AppCompatActivity() {
 
@@ -19,7 +16,7 @@ class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.signup)
+        setContentView(R.layout.activity_sign_up)
 
 
         }
