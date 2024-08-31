7ea92196e52cf1f99274237c44f5ce356ab97f6c
PeynBut <151439527+PeynBut@users.noreply.github.com>
2024-08-31 09:51:58 +0800

    btn ud

--- app/src/main/java/com/example/kotlinproject/ForgotPassword.kt
+++ app/src/main/java/com/example/kotlinproject/ForgotPassword.kt
@@ -1,20 +1,24 @@
 package com.example.kotlinproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ForgotPassword : AppCompatActivity() {
+
+    val btnConfirm = findViewById<Button>(R.id.btnConfirm)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_forgot_password)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        btnConfirm.setOnClickListener { //ConfirmBtn
+            val intent = Intent(this, LogInActivity::class.java)
         }
     }
 }
